d09d36cb543780a73b13ef8bbebc1a95735d1356
Alfonso Antonio <j.alfonso.antonio@gmail.com>
2017-08-08 12:55:36 -0400

    added css

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const mustacheExpress = require('mustache-express');
-const expressValidator = require('express-validator');
 const todoRoutes = require('./todo');
 const app = express();
 
@@ -13,11 +12,10 @@ app.set('views', __dirname + '/views')
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(expressValidator())
 
 //Route
 app.use(todoRoutes);
 
 app.listen(3000, () => {
-  console.log("WORKING");
+  console.log("localhost://3000");
 })
